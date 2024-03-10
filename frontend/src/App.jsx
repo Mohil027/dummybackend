2e934740bee21643e05fb6c2a7232c7f09c60015
Mohil Khimani <mohil.khimani@gmail.com>
2024-03-10 15:07:09 -0700

    some cors issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,25 +8,28 @@ function App() {
   const [jokes, setJokes] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:3000/jokes')
+    axios.get('http://localhost:3000/api/jokes')
     .then((response) => {
       setJokes(response.data)
     })
     .catch((error) => {
       console.log(error);
     })
-  })
+  }, []);
+  console.log(jokes);
   return (
     <>
       <h1>Hello Frontend here!</h1>
       <p>Jokes: {jokes.length}</p>
-
+      
       {
         jokes.map((joke, index) => {
-          <div key={joke.id}>
-            <h3>{joke.title}</h3>
-            <p>{joke.content}</p>
-          </div>
+          return ( 
+            <div className="rough" key={joke.id}>
+              <h3>{joke.title}</h3>
+              <p>{joke.content}</p>
+            </div>
+          );
 
         })
       }
